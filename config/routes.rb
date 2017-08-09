8744ef2d7fc5fd10c09f141cd19d7182584d1d54
ilduchea <ilduchea@gmail.com>
2017-08-09 16:42:14 -0700

    Add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :game_systems do
+    resources :characters, except: [:index]
+  end
+
+  resources :sections, except: [:index] do
+    resources :traits, except: [:index, :show]
+  end
 end
